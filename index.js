60340891a7ded2b9630a2592ba3a0dc2a3c38b6b
Ki Jong Byun <kjbyun0@gmail.com>
2023-07-04 14:49:54 -0500

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(elem, idx, arr) {
+        return elem.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter((elem, idx, arr) => elem.slice(0, name.length) === name);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((elem, idx, arr) => elem.name === name);
+}
